8b13f3a21254f183af5ccd1135f3221f65ca6b4f
StiefelKing <der@getzler.de>
2020-05-14 08:47:33 +0200

    more cleanup

--- IbanCalculatorExtremePY/IbanCalculatorExtremePY.py
+++ IbanCalculatorExtremePY/IbanCalculatorExtremePY.py
@@ -1,11 +1,13 @@
 import sys
 import os
 
+
 class Kto:
     seed = 0
     checksum = 0
     value = 0
-    def __init__(self, seed,):
+
+    def __init__(self, seed, ):
         self.seed = seed
         if seed > 999999999 or seed < 1:
             raise ValueError
@@ -15,7 +17,7 @@ class Kto:
             checksum = 0
             # special case
             j = 0
-            if array [2] == 9:
+            if array[2] == 9:
                 j = 7
             else:
                 j = 6
@@ -23,9 +25,9 @@ class Kto:
             m = 2
             sum = 0
             for x in range(j):
-                array[8-x] = array[8-x] * m
+                array[8 - x] = array[8 - x] * m
                 m += 1
-                sum = sum + array[8-x]
+                sum = sum + array[8 - x]
 
             # final
             checksum = 11 - sum % 11
@@ -34,15 +36,20 @@ class Kto:
             self.checksum = checksum
             self.value = (str(seed).zfill(9) + str(checksum))
 
+
 class IBAN:
     kto = 0
     value = ""
+
     def __init__(self, kto, blz):
         country = 131400
         countryStr = "DE"
         self.kto = kto
-        cc = 98 - (((blz*10000000000000000) + (int(kto.value)*1000000) + country) % 97)
-        self.value = "DE" + str(cc).zfill(2) + " " + str(blz).zfill(8)[0:4] + " " + str(blz).zfill(8)[4:8] + " " + str(kto.value).zfill(10)[0:4] + " " + str(kto.value).zfill(10)[4:8] + " " + str(kto.value).zfill(10)[8:10]
+        cc = 98 - (((blz * 10000000000000000) + (int(kto.value) * 1000000) + country) % 97)
+        self.value = "DE" + str(cc).zfill(2) + " " + str(blz).zfill(8)[0:4] + " " + str(blz).zfill(8)[4:8] + " " + str(
+            kto.value).zfill(10)[0:4] + " " + str(kto.value).zfill(10)[4:8] + " " + str(kto.value).zfill(10)[8:10]
+
+
 while 1:
     try:
         print("IBAN ausgeben von: ")
@@ -53,9 +60,9 @@ while 1:
         sys.exit(0)
     if os.path.exists("List.txt"):
         os.remove("List.txt")
-    f = open("List.txt","a")
+    f = open("List.txt", "a")
     f.write("IBAN List for 73369920 von " + str(start) + " bis " + str(end) + "\n")
-    for i in range(start, end+1):
+    for i in range(start, end + 1):
         iban = IBAN(Kto(i), 73369920)
         out = iban.kto.value + " | " + iban.value
         print(out)
