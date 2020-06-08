1dddd641012bf7d9a20b81cdf8469357d9a53761
StiefelKing <der@getzler.de>
2020-06-08 17:31:17 +0200

    Update PDFMerge.py

--- PDFMerge/PDFMerge.py
+++ PDFMerge/PDFMerge.py
@@ -7,18 +7,25 @@ class Attachment:
     filename = ""
     notary = ""
     number = 0
-    year = 0
+    year4 = 0
+    year2 = 0
 
     def __init__(self, filepath):
         self.filepath = filepath
         self.filename = os.path.basename(filepath)
         x = self.filename.split("-")
-        self.year = x[0]
+        self.year4 = x[0]
+        self.year2 = self.year4[:2]
         self.notary = x[1]
         self.number = x[2].split(".")[0]
 
     def getdocument(self):
         print("stub")
+        if self.notary == "C":
+            folder = "U:\\Caspary\\{}{}\\".format(self.notary, self.year4)
+            file = "{}{}_{}".format(self.notary, self.number, self.year2)
+            print(folder)
+            print(file)
         # todo returns filepath of deed for attachment
 
 
@@ -39,8 +46,7 @@ def merge_pdfs(paths, output):
 if __name__ == '__main__':
     paths = ['U:\\Caspary\\C2020\\C0487_20Pl.tlw.verkl.pdf',
              'G:\\Notarstelle_Sonthofen\\Urkundenanlagen\\2020-C-0487.pdf']
-    Attachment(paths[0], False)
-    Attachment(paths[1], True)
+    Attachment(paths[1]).getdocument()
     # merge_pdfs(paths, output='merged.pdf')
 
 # todo sorting through Filesystem get Year, Notary and Number from Anlage and Search in Ablage --> Ununified Filenames
